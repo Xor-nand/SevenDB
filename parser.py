57bcf01a4069b802f23dcaa6caa40edbbc08ec8b
Xor-nand <tommasoascari@gmail.com>
2020-11-03 13:40:13 +0100

    first update of parser for email body

--- parser.py
+++ parser.py
@@ -16,12 +16,29 @@ def check(str, pattern):
     else:
         trash.append(str)
 
+def sprit(prodline):
+    prod = prodline.strip().split('  ')
+    res = []
+    for ele in prod:
+        if ele.strip():
+            res.append(ele.strip())
+    return(res)
+
 file = "outputmail.txt"
 listline = linesof(file)
 listofprods = []
 trash = []
+box = []
 
 pattern = re.compile('\d[.]\d+\s')
 
 for i in listline:
      check('{}'.format(i), pattern)
+
+for prod in listofprods:
+    box.append(sprit(prod))
+
+boxx = box.pop(0)
+
+for item in box:
+    print ("OGGETTO: {}, PREZZO: {}, QT: {}".format(item[0],item[3],item[2]))
